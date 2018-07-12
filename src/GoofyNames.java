9545a51adfe68e26774888d863597685e4ef4589
league <league@wts7.attlocal.net>
2018-07-11 18:26:28 -0700

    goofynames

--- src/GoofyNames.java
+++ src/GoofyNames.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright (c) The League of Amazing Programmers 2013-2017
+ *    Copyright (c) The League of Amazing Programmers 2013-2018
  *    Level 0
  */
 
@@ -13,22 +13,21 @@ public class GoofyNames {
 
 		System.out.println(name.toUpperCase());
 
-		// 3. Loop through each character of the name (steps 4 - 6).
-		//    HINT: Use .length() to determine the number of characters in the String.
-
-			// 4. Create a String variable to store the next character of the name
-			//    using .substring(start, end)
-			//    HINT: replace 'start' and 'end' to get String with character at i 
-			
-			// 5. Use MODULO to set this variable to upper case for EVEN characters
-			//    and lower case for ODD characters.
-
-			
-			// 6. ADD this String  (containing 1 char) to the goofyName String
-
+		for(int i=0; i<name.length(); i++) {
+			String cha=name.substring(i,i+1);
+			if(i%2 ==0) {
+				cha=cha.toUpperCase();
+			}
+			else {
+				cha=cha.toLowerCase();
+				
+			}
+			goofyName+=cha;
+		}
+		System.out.println(goofyName);
 		
-		// 7. Use pop-up to show user their Goofy name
-
+	
+		
 	}
 }
 
--- src/GuessingPi.java
+++ src/GuessingPi.java
@@ -6,8 +6,9 @@
 import javax.swing.JOptionPane;
 
 public class GuessingPi {
-	// 1. Make a main method and make sure your program can run
-
+	public static void main(String[] args) {
+		String Pi
+	}
 		// 2. Make a String variable to hold the value of Pi.
 		//    Get the first few digits from http://www.piday.org/million/.
 
