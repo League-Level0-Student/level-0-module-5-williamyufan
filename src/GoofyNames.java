33c780dca25dc21f6a948d937697eddfa7354e8b
league <league@wts7.attlocal.net>
2018-07-11 18:25:46 -0700

    goofynames

--- src/GoofyNames.java
+++ src/GoofyNames.java
@@ -9,10 +9,9 @@ public class GoofyNames {
 	public static void main(String[] args) {
 		String goofyName = "";
 
-		// 1. Ask the user to enter their name
+		String name=JOptionPane.showInputDialog(null, "What is your FULL name?");
 
-		// 2. Print upper case name to the console using .toUpperCase()
-		//    Run your program to see that this works.
+		System.out.println(name.toUpperCase());
 
 		// 3. Loop through each character of the name (steps 4 - 6).
 		//    HINT: Use .length() to determine the number of characters in the String.
