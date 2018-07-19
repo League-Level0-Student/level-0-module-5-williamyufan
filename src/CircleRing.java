6897c1fe1189bb021ead53e6c1b5b8441a0ebbc1
league <league@wts7.attlocal.net>
2018-07-18 18:27:41 -0700

    circlerings

--- src/CircleRing.java
+++ src/CircleRing.java
@@ -8,6 +8,17 @@ import org.jointheleague.graphical.robot.Robot;
 public class CircleRing {
     public static void main(String[] args) throws Exception {
 
+    	Robot bob=new Robot();
+    	bob.setX(150);
+    	bob.setY(200);
+    	bob.penDown();
+    	bob.setSpeed(10);
+    	for(int i=0; i<=360; i++) {
+    		bob.move(3);
+    		bob.turn(1);
+    		
+    	}
+    	
         // 1. Make a new Robot
 
 
