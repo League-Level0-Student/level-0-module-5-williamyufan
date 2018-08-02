f2e245e25047d493cfb425324ad119eda639d5b4
league <league@imac-8.attlocal.net>
2018-08-01 18:26:32 -0700

    forloop stuff

--- src/forloopgauntlet.java
+++ src/forloopgauntlet.java
@@ -23,6 +23,28 @@ for(int b=1; b<=99; b++) {
 		System.out.println(b);
 	}
 }
+for(int v=1; v<=500; v++) {
+	if(v%2==0) {
+		System.out.println("even");
+	}
+	else {
+		System.out.println("odd");
+	}
+}
+for(int h=0; h<=777; h++) {
+	if(h%7==0) {
+		System.out.println(h);
+	}
+}
+for(int j=2006; j<=2018; j++) {
+	System.out.println("in " +j+", I was"+(j-2006) +"years old");
+}
+
+for(int g=0; g<=2; g++) {
+	for(int o=0; o<=2; o++) {
+		
+	}
+}
 
 	
 	}
